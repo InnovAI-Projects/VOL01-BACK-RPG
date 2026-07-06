d346a03f08f0e84afef273559911e60d0577ecfb
luismutton <lfmutton@gmail.com>
2026-07-06 12:29:22 -0300

    Feat: Adicionando Swagger

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck } from '@nestjs/terminus';
 import { HealthService } from './health.service';
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('health')
 @Controller('health')
 export class HealthController {
   constructor(private healthService: HealthService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Verifica estabilidade da conexão com o banco.' })
+  @ApiResponse({ status: 200, description: 'Banco online' })
+  @ApiResponse({ status: 503, description: 'Banco offline' })
   @HealthCheck()
   check() {
     return this.healthService.healthCheck();
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('VOL01-BACK-RPG')
+    .setDescription('The rpg manager API and it´s endpoints')
+    .setVersion('1.0')
+    .addTag('rpg')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  SwaggerModule.setup('api/swagger', app, documentFactory);
   await app.listen(process.env.PORT!);
 }
 bootstrap();
